24dabfdb42efb5d53dc9944a85404e71654cc499
Bagus Rizky M <mohamad.15@students.amikom.ac.id>
2023-01-11 23:56:52 +0700

    splash screen and viewpager

---
+++ app/src/main/java/com/example/splash_android/SplashActivity.kt
@@ -0,0 +1,16 @@
+package com.example.splash_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.splash_android.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
